f0e1dfca1bf3715b440c7f9f1494b9bb04c4182a
Alan Wong <pyepye@gmail.com>
2014-06-15 00:54:42 -0700

    added the Stats object

--- TypingGame/TypingGame/Form1.Designer.cs
+++ TypingGame/TypingGame/Form1.Designer.cs
@@ -53,6 +53,11 @@
             this.listBox1.Size = new System.Drawing.Size(856, 131);
             this.listBox1.TabIndex = 0;
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
--- TypingGame/TypingGame/Form1.cs
+++ TypingGame/TypingGame/Form1.cs
@@ -12,9 +12,24 @@ namespace TypingGame
 {
     public partial class Form1 : Form
     {
+        public Stats stats = new Stats();
+        public Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Add a random key to the ListBox
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+            if (listBox1.Items.Count > 7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Game over");
+                timer1.Stop();
+            }
         }
     }
 }
